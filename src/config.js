20c24e82c9c52ce75875946fe35e6301b037d196
jacobsomer <jacobsomer@users.noreply.github.com>
2020-06-06 12:38:38 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-11"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://jpy87a1lqj.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_OJ9JwGAdX",
+    APP_CLIENT_ID: "7gpo5k22459go38kaesh2gq2kn",
+    IDENTITY_POOL_ID: "us-east-1_OJ9JwGAdX"
+  }
+};
