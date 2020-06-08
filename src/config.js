c3f250a5400cc984b9590fed6e2c0fffd85bff26
jacobsomer <jacobsomer@users.noreply.github.com>
2020-06-08 14:41:48 -0400

    Adding a Netlify build script

--- src/config.js
+++ src/config.js
@@ -1,3 +1,45 @@
+const dev = {
+  STRIPE_KEY: "pk_test_2ZpquUmoGp1CrkYb2h3lRZYO00DeZq7tUd",
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-1c3s0xm2d4y51"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://rvsko4ld2h.execute-api.us-east-1.amazonaws.com/dev/"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_hrTLr25Kc",
+    APP_CLIENT_ID: "24jdgcofq002nd4r4rkfgtcgm7",
+    IDENTITY_POOL_ID: "us-east-1:524701b0-4572-4666-b576-265d20f2fea0"
+  }
+};
+
+const prod = {
+  STRIPE_KEY: "pk_test_2ZpquUmoGp1CrkYb2h3lRZYO00DeZq7tUd",
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-1c3s0xm2d4y51"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://sn3zqjc2k5.execute-api.us-east-1.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_h1GjgCvMt",
+    APP_CLIENT_ID: "mfgh695m9utr8bk5qp5riqv55",
+    IDENTITY_POOL_ID: "us-east-1:bd866436-a356-41e4-af64-5698ec54340b"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+
 export default {
   STRIPE_KEY: "pk_test_2ZpquUmoGp1CrkYb2h3lRZYO00DeZq7tUd",
   MAX_ATTACHMENT_SIZE: 5000000,
