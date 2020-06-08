774f4fdeaf895d1365b5102d87ba9626508532e8
jacobsomer <jacobsomer@users.noreply.github.com>
2020-06-08 11:26:37 -0400

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 export default {
+  STRIPE_KEY: "pk_test_2ZpquUmoGp1CrkYb2h3lRZYO00DeZq7tUd",
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-uploads-11"
+    BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-6vdjbw0u32ag"
   },
   apiGateway: {
     REGION: "us-east-1",
@@ -11,6 +13,6 @@ export default {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_OJ9JwGAdX",
     APP_CLIENT_ID: "7gpo5k22459go38kaesh2gq2kn",
-    IDENTITY_POOL_ID: "us-east-1_OJ9JwGAdX"
-  }
+    IDENTITY_POOL_ID: "us-east-1:6b3424c5-774f-4f20-af71-e96b82ec89ec"
+  },
 };
